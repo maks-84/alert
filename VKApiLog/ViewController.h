846f4ede261a518331c52eae62a74578b0602267
Maks <m.filinskiy@gmail.com>
2016-06-08 22:34:58 +0300

    Initial Commit

---
+++ VKApiLog/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  VKApiLog
+//
+//  Created by Mac on 08.06.16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(void) alertView;
+
+@end
+
---
+++ VKApiLog/ViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  VKApiLog
+//
+//  Created by Mac on 08.06.16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void) viewDidAppear:(BOOL)animated {
+    [self alertView];
+}
+-(void) alertView {
+        UIAlertController *alertView  = [UIAlertController alertControllerWithTitle:@"Enter VK" message:@"Log In" preferredStyle:UIAlertControllerStyleAlert];
+    
+       [alertView addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"log in";
+    }];
+    [alertView addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"passwotd";
+        textField.secureTextEntry = YES;
+    }];
+    UIAlertAction *doneButton = [UIAlertAction actionWithTitle:@"Log in" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        UITextField *login = alertView.textFields[0].text;
+        UITextField *pass = alertView.textFields[1].text;
+        
+        NSLog(@"log in%@ pass:%@", login, pass);
+        
+    }];
+    UIAlertAction *cancelButton = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alertView addAction:doneButton];
+    [alertView addAction:cancelButton];
+    
+
+    [self presentViewController:alertView animated:YES completion:nil];
+    
+    NSProcessInfo 
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
